18e4dcedcb819de7e47d8eb4cf715c88837ac2cc
hassan <nikkarhossein@gmail.com>
2023-06-20 08:22:38 +0330

    Regular expression project "find phone number and email address" is added and tested

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import phoneNumberAndEmailExtractor
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -12,7 +14,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+    '''
 #Regular Expressions prefunctions
     message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
     message1='Batmobile lost a wheel'
@@ -26,3 +28,17 @@ if __name__ == '__main__':
     RegularExpressions.nameFinder(nameString)
     RegularExpressions.dotAllMatch(dotAllString)
     RegularExpressions.nameSub(subString)
+'''
+#-------------------------------------Project
+    messageProject = 'Call me at 415-555-1011 or 415 555-1011 tomorrow. (415)555-9999 and (415) 555-9999is my office.or ' \
+                     'email abc123@gmail.com'
+    #phoneNumbers=phoneNumberAndEmailExtractor.phoneNumberFinder(messageProject)
+    #[print(f'Phone number {num}') for num in phoneNumbers]
+    #[print(num[0]) for num in phoneNumbers]
+    #print(phoneNumbers)
+
+    #emailAddres=phoneNumberAndEmailExtractor.emailAddrFinder(messageProject)
+    #print(emailAddres)
+
+    matches=phoneNumberAndEmailExtractor.phoneAndEmailFinder()
+    #print(matches)
